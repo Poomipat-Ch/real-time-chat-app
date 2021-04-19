22932891894b5ad332eee96be1f24be3b68e1cc2
Poomipat-Ch <poomipat002@gmail.com>
2021-04-19 17:07:24 +0700

    ✨✨✨

---
+++ backend/server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const app = express();
+const socket = require("socket.io");
+require("colors");
+const { userJoin, getCurrentUser, userLeave } = require("./dummyuser");
+
+const server = app.listen(process.env.PORT || 5050, () => {
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} on port ${
+      process.env.PORT || 5050
+    }`.yellow.bold
+  );
+});
+
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  //when new user join room
+  socket.on("joinRoom", ({ username, roomname }) => {
+    // create user
+    const user = userJoin(socket.id, username, roomname);
+    console.log(socket.id, "=id");
+    socket.join(user.room);
+
+    // emit message to user to welcome
+    socket.emit("message", {
+      userId: user.id,
+      username: user.username,
+      text: `Welcome ${user.username}`,
+    });
+
+    // Broadcast message to everyone except sender
+    socket.broadcast.to(user.room).emit("message", {
+      userId: user.id,
+      username: user.username,
+      text: `${user.username} has joined the chat`,
+    });
+  });
+
+  //when somebody send message
+  socket.on("chat", (text) => {
+    // Get user room and emit message
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit("message", {
+      userId: user.id,
+      username: user.username,
+      text: text,
+    });
+  });
+
+  // Disconnet, when user leave room
+  socket.on("disconnect", () => {
+    // delete user from users and emit that user left the chat
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", {
+        userId: user.id,
+        username: user.username,
+        text: `${user.username} has left the chat`,
+      });
+    }
+  });
+});
